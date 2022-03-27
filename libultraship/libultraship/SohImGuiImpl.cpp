a84f2a75828ba6619893b790754806e9977403e4
Andrew Piper <andrew@threax.com>
2022-03-27 16:31:24 -0400

    Remove separate ui menu.

--- libultraship/libultraship/SohImGuiImpl.cpp
+++ libultraship/libultraship/SohImGuiImpl.cpp
@@ -333,9 +333,6 @@ namespace SohImGui {
                     needs_save = true;
                 }
 
-                ImGui::Text("UI");
-                ImGui::Separator();
-
                 if (ImGui::Checkbox("Minimal UI", &Game::Settings.enhancements.minimal_ui)) {
                     CVar_SetS32(const_cast<char*>("gMinimalUI"), Game::Settings.enhancements.minimal_ui);
                     needs_save = true;
